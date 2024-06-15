4064c4a33e29485c7d8bf321d4f388e57ed3c7f3
rajaazhagu <azhaguazhagu30@gmail.com>
2024-06-15 11:55:15 +0530

    Your commit message

--- client/src/pages/Contactus.js
+++ client/src/pages/Contactus.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Contact = () => {
   return (
     <div className='flex flex-col gap-4 my-10 text-1xl font-bold justify-center items-center'>
+      <h1>DualDealMart</h1>
       <p>contact:9791818045</p>
       <p>Email:secretsquare302@gmail.com</p>
+      <p>East street,uppparpatti,Theni</p>
     </div>
     
   )
