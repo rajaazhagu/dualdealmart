496eb61b16c02462741126e0b74d216567b17274
rajaazhagu <azhaguazhagu30@gmail.com>
2024-06-17 17:27:38 +0530

    Your commit message

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -43,7 +43,7 @@ const Payment = () => {
       if (sessionId) {
         const checkOutOptions = {
           paymentSessionId: sessionId,
-          redirectTarget: '_modal'
+          redirectTarget: '_self'
         };
 
         cashfree.checkout(checkOutOptions).then((response) => {
