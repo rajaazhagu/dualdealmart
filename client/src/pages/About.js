1caad5cecad021e9ec55402a87f1ef622adc8964
rajaazhagu <azhaguazhagu30@gmail.com>
2024-06-19 17:39:59 +0530

    Your commit message

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,8 +4,9 @@ const About = () => {
   return (
     <div>
       <h1 className='text-3xl font-bold text-black mb-4'>Welcome to DualDealMart!!</h1>
-      <p className='mb-4 text-slate-700'>At DualDealMart, we strive to connect people with the best products available for rent and sale. Whether you're looking to buy your dream home, rent a vehicle for a weekend getaway, or find the right equipment for your next big project, we have you covered.</p>
-      <p className='text-slate-700'>Our mission is to provide a seamless and trustworthy platform where users can find, compare, and acquire products for rent or purchase. We aim to offer a diverse range of high-quality products, ensuring that every user can find exactly what they need.</p>
+      <p className='mb-4 text-slate-700 font-semibold'>At DualDealMart, we strive to connect people with the best products available for rent and sale. Whether you're looking to buy your dream home, rent a vehicle for a weekend getaway, or find the right equipment for your next big project, we have you covered.</p>
+      <h1 className='text-3xl font-bold text-black mb-4'>Mission!!</h1>
+      <p className='text-slate-700 font-semibold'>Our mission is to provide a seamless and trustworthy platform where users can find, compare, and acquire products for rent or purchase. We aim to offer a diverse range of high-quality products, ensuring that every user can find exactly what they need.</p>
     </div>
   )
 }
