6c2e88e90f9bb58559980f7f5556589cac7c8027
rajaazhagu <azhaguazhagu30@gmail.com>
2024-06-19 22:02:24 +0530

    Your commit message

--- client/src/pages/Description.js
+++ client/src/pages/Description.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Description = ({list}) => {
   return (
     <div className='flex justify-center items-center'>
-            <div className="w-72 h-36 border bg-white  break-words ml-4 my-3 rounded-lg">
+            <div className="w-72 h-45 border bg-white  break-words ml-4 my-3 overflow-scroll rounded-lg">
                 <p className='text-black font-semibold ml-2'>Description : </p>
                 <p className='ml-8'>{list[0].description}</p>
             </div>
