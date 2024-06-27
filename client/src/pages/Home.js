03e02e681d5b031ac0ebc42c748db812316b7d6e
rajaazhagu <azhaguazhagu30@gmail.com>
2024-06-27 18:14:07 +0530

    Your commit message

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import { FaStar } from 'react-icons/fa';
 const Home = ({list,user}) => {
   const [offer,setOffer] = useState([])  
   const [noOffer,setNoffer] =useState([]) 
+  const [premium,setPremium] =useState([]) 
   const dateMonth = new Date()
  /* const handlePost =(async()=>{
     try {
@@ -54,11 +55,13 @@ const Home = ({list,user}) => {
 
   useEffect(()=>{
     const offerProducts = list.filter((single)=>single.offer===true)
-    const noofferProducts = list.filter((single)=>single.offer!=true)
+    const noofferProducts = list.filter((single)=>single.offer!==true && !single.date)
+    const premiumProducts = list.filter((single)=>single.date)
     setNoffer(noofferProducts)
     setOffer(offerProducts)
+    setPremium(premiumProducts)
   },[list])
- 
+
   return (
     <div>
       <div className='flex flex-col my-10 gap-6 px-3 max-w-6xl mx-auto'>
@@ -69,6 +72,52 @@ const Home = ({list,user}) => {
           <h2 className='font-semibold text-xl'>Pay <span className='text-black'>one </span> time for unlimited listings</h2><span className='text-black font-bold my-2 text-xl'>Rent it,Own it,Love it...</span>
         </div>
       </div>
+      <h1 className='flex justify-center font-bold text-slate-700 drop-shadow-2 text-2xl my-4'>Top products !!</h1>
+      {
+  premium.length > 0 ? (
+    <div className='h-100 w-100 flex overflow-x-scroll no-scrollbar'>
+      <div className='flex flex-row h-90 sm:flex-row'>
+        {premium.slice(0).reverse().map((single) => (
+          <Link to={`/listing/${single._id}`} key={single._id}>
+            <div className='shadow-lg border h-50 border-slate-200 bg-white rounded-lg mx-7 flex flex-col gap-1 my-2'>
+              <img className='w-80 h-80 sm:w-40 h-40 self-center rounded-lg shadow-lg z-5' src={single.imageURLs[0]} alt='Offer' />
+              <h1 className='text-slate-600 text-center'>
+                <span className='text-black text-1xl font-bold'>Category : </span>
+                {single.name.slice(0, 7)}
+                {single.name.length > 9 && '...'}
+              </h1>
+              <h1 className='text-slate-600 text-center'>
+                <span className='text-black text-1xl relative font-bold'>Price : </span>
+                Rs.{single.price}
+              </h1>
+              <div className='flex flex-row gap-2 justify-center relative bottom-2'>
+                <p className='bg-red-900 my-2 text-white text-center w-20 h-5 rounded-lg'>
+                  {single.type === 'rent' ? 'For Rent' : 'For Sale'}
+                </p>
+                {single.offer && (
+                  <p className='bg-green-900 w-20 text-white h-5 text-center rounded-lg my-2'>offer</p>
+                )}
+              </div>
+              <div className='flex flex-col'>
+                <h1 className='text-center font-bold'>Review</h1>
+                <span className='text-2xl font-bold text-center'>
+                  {single.totalReview > 0 && single.totalReview <= 5 ? (single.totalReview).toFixed(1) : single.totalReview > 5 ? 5 : 0}
+                </span>
+                <div className='flex gap-4 justify-center'>
+                  {[...Array(5)].map((_, index) => (
+                    <FaStar key={index} size={20} color={index + 1 <= single.totalReview ? 'gold' : 'grey'} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  ) : (
+    ""
+  )
+}
       <h1 className='flex justify-center font-bold text-slate-700 drop-shadow-2 text-2xl my-4'>offer products !!</h1>
       {
   offer.length > 0 ? (
